d8de5d6bcc41c055b5a8e448d3afc72c0d52a0db
mm99dd99 <mm99dd99.wk@gmail.com>
2017-04-26 13:53:23 -0500

    add scrollview on register page and create challenge page add logout on sopnsor setting page

--- app/src/main/java/com/example/dustin/cs495helloworld/Set_sponsor.java
+++ app/src/main/java/com/example/dustin/cs495helloworld/Set_sponsor.java
@@ -1,8 +1,11 @@
 package com.example.dustin.cs495helloworld;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Set_sponsor extends AppCompatActivity {
 
@@ -10,6 +13,19 @@ public class Set_sponsor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_sponsor);
+        final Button btnLogout = (Button) findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = SaveSharedPreference.getSharedPreferences(v.getContext()).edit();
+                editor.clear(); //clear all stored data
+                editor.commit();
+
+                Intent nextScreen = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(nextScreen, 0);
+            };
+        });
+
     }
 
 
